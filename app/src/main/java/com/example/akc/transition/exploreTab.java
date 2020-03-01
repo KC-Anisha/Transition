e1cf472eb1119c961284b8bfd495eb43f3418b23
Anisha KC <anisha.kc7219@gmail.com>
2020-03-01 06:08:37 -0500

    Message for Notification tab and added some content to the explore tab

--- app/src/main/java/com/example/akc/transition/exploreTab.java
+++ app/src/main/java/com/example/akc/transition/exploreTab.java
@@ -1,5 +1,6 @@
 package com.example.akc.transition;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -21,6 +22,13 @@ public class exploreTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+//        container.findViewById(R.id.gender).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(exploreTab.this, exploreActivity.class));
+//            }
+//        });
         return inflater.inflate(R.layout.fragment_explore_tab, container, false);
     }
 }
