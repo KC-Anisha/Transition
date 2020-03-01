c1612bf26e12febf8ff792ed31a974c60bbe909c
Alexis Indick <aindick@oswego.edu>
2020-03-01 00:12:36 -0500

    Added Explore button sorta

--- app/src/main/java/com/example/akc/transition/Explore.java
+++ app/src/main/java/com/example/akc/transition/Explore.java
@@ -3,6 +3,8 @@ package com.example.akc.transition;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -10,11 +12,15 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class Explore extends AppCompatActivity {
+    EditText explore;
+    Button explore1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explore);
+
+        explore = findViewById(R.id.explore1);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
