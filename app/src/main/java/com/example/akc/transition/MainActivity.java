ebeb0dff10eeb45e13627c931f221e2871240d48
Anisha KC <anisha.kc7219@gmail.com>
2020-03-01 05:45:23 -0500

    Created Functional Tabs

--- app/src/main/java/com/example/akc/transition/MainActivity.java
+++ app/src/main/java/com/example/akc/transition/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TableLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +19,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        exploreTab = (TabItem) findViewById(R.id.Explore);
+        notificationsTab = (TabItem) findViewById(R.id.Notifications);
+        viewPager = findViewById(R.id.viewpager);
+
+        pagerAdapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                if (tab.getPosition() == 0){
+                    pagerAdapter.notifyDataSetChanged();
+                } else if (tab.getPosition() == 1){
+                    pagerAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
     }
 }
--- app/src/main/java/com/example/akc/transition/PageAdapter.java
+++ app/src/main/java/com/example/akc/transition/PageAdapter.java
@@ -1,4 +1,40 @@
 package com.example.akc.transition;
 
-public class PageAdapter {
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class PageAdapter extends FragmentPagerAdapter {
+
+    private int numOfTabs;
+
+    public PageAdapter(FragmentManager fm, int numOfTabs) {
+        super(fm);
+        this.numOfTabs= numOfTabs;
+    }
+
+
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new exploreTab();
+            case 1:
+                return new notificationTab();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return numOfTabs;
+    }
+
+    @Override
+    public int getItemPosition(@NonNull Object object) {
+        return POSITION_NONE;
+    }
 }
